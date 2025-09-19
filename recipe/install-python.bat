6f5da5ede1f7745ca101c5508f093712466f1fed
Tyler Erickson <tyler@vorgeo.com>
2025-09-19 16:25:42 -0700

    Remove install scripts

--- recipe/install-python.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-REM Install the Quarto Python API package
-pip install quarto-cli --no-deps --ignore-installed --no-cache-dir -vvv
-if errorlevel 1 exit 1
--- recipe/install-python.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Install the Quarto Python API package
-pip install quarto-cli --no-deps --ignore-installed --no-cache-dir -vvv
